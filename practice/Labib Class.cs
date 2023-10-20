ccff587ac028b6632628068d6c0e8284ff1ddae6
KashshafLabib <kashshaflabib@iut-dhaka.edu>
2023-10-20 18:50:32 +0600

    Labib class added

---
+++ practice/Labib Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    public class Labib_Class
+    {
+        public string name = "Kashshaf Labib";
+        public int id = 210042151;
+    }
+}
